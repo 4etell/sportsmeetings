a73c048ae0c49894583b3053f1cba84ed625dd23
horriblehades <daniilkaranov@yandex.ru>
2021-10-17 21:56:25 +0300

    refactor: AdminMeetingCategoryReqDto

--- src/main/java/com/foretell/sportsmeetings/dto/req/AdminMeetingCategoryReqDto.java
+++ src/main/java/com/foretell/sportsmeetings/dto/req/AdminMeetingCategoryReqDto.java
@@ -2,7 +2,12 @@ package com.foretell.sportsmeetings.dto.req;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 public class AdminMeetingCategoryReqDto {
+    @NotBlank(message = "Name cannot be blank")
+    @Size(message = "Max length of name is 100")
     private String name;
 }
