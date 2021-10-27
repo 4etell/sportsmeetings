17b85c497a3e3ca6a9c0e5f5ec4c6f301db1888d
horriblehades <daniilkaranov@yandex.ru>
2021-10-28 00:35:54 +0300

    feat: activate TelegramBot

---
+++ src/main/java/com/foretell/sportsmeetings/controller/rest/TelegramBotRestController.java
@@ -0,0 +1,31 @@
+package com.foretell.sportsmeetings.controller.rest;
+
+import com.foretell.sportsmeetings.dto.res.TelegramBotActivationCodeResDto;
+import com.foretell.sportsmeetings.service.UserService;
+import com.foretell.sportsmeetings.util.jwt.JwtProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("telegram-bot")
+public class TelegramBotRestController {
+
+    private final JwtProvider jwtProvider;
+    private final UserService userService;
+
+    public TelegramBotRestController(JwtProvider jwtProvider, UserService userService) {
+        this.jwtProvider = jwtProvider;
+        this.userService = userService;
+    }
+
+    @GetMapping("activation-code")
+    public TelegramBotActivationCodeResDto getActivationCode(HttpServletRequest httpServletRequest) {
+        String usernameFromToken =
+                jwtProvider.getUsernameFromToken(jwtProvider.getTokenFromRequest(httpServletRequest));
+
+        return userService.getTelegramBotActivationCode(usernameFromToken);
+    }
+}
